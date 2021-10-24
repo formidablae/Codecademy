529a57a569ff5d0b1f6af3df515effe42ef2adfa
formidablae <81068781+formidablae@users.noreply.github.com>
2021-10-24 03:44:04 +0200

    Started Learn PHP course in Codecademy

---
+++ Learn_PHP/01_Getting_Started_with_PHP/01.1_Introduction_to_PHP/01.1.3_PHP_Comments/index.php
@@ -0,0 +1,9 @@
+<?php
+// I will always remember this
+echo "Hello, World!"; # My first PHP statement
+
+/* "I've never thought of PHP as more 
+than a simple tool to solve problems."
+- Rasmus Lerdorf */
+echo "\nRasmus is the creator of PHP!";
+
