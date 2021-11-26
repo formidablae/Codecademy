cefb93d8315974e696054eda969566bd0e04069e
formidablae <81068781+formidablae@users.noreply.github.com>
2021-11-26 22:33:40 +0100

    Did Career Path Full-Stack Engineer / 03 JavaScript Syntax Part I / 03.5 Learn JavaScript Syntax Functions / 03.5.1 Functions

---
+++ Career_Path_Full-Stack_Engineer/03_JavaScript_Syntax_Part_I/03.5_Learn_JavaScript_Syntax_Functions/03.5.1_Functions/03.5.1.4_Default_Parameters/main.js
@@ -0,0 +1,5 @@
+function makeShoppingList(item1 = 'milk', item2 = 'bread', item3 = 'eggs') {
+    console.log(`Remember to buy ${item1}`);
+    console.log(`Remember to buy ${item2}`);
+    console.log(`Remember to buy ${item3}`);
+}
