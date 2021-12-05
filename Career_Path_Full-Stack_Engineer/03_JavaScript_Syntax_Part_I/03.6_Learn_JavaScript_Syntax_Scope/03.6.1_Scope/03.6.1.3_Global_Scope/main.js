7d91f11bc9c18625d888f1a257e4a6b2e576fd89
formidablae <81068781+formidablae@users.noreply.github.com>
2021-12-05 03:36:34 +0100

    Did Career Path Full-Stack Engineer / 03 JavaScript Syntax Part I / 03.6 Learn JavaScript Syntax Scope

---
+++ Career_Path_Full-Stack_Engineer/03_JavaScript_Syntax_Part_I/03.6_Learn_JavaScript_Syntax_Scope/03.6.1_Scope/03.6.1.3_Global_Scope/main.js
@@ -0,0 +1,8 @@
+const satellite = 'The Moon';
+const galaxy = 'The Milky Way';
+const stars = 'North Star';
+
+function callMyNightSky() {
+    return 'Night Sky: ' + satellite + ', ' + stars + ', and ' + galaxy;
+}
+console.log(callMyNightSky());
---
+++ Career_Path_Full-Stack_Engineer/03_JavaScript_Syntax_Part_I/03.6_Learn_JavaScript_Syntax_Scope/03.6.1_Scope/03.6.1.6_Practice_Good_Scoping/main.js
@@ -0,0 +1,13 @@
+const logVisibleLightWaves = () => {
+    let lightWaves = 'Moonlight';
+    let region = 'The Arctic';
+    // Add if statement here:
+    if (region === 'The Arctic') {
+        let lightWaves = 'Northern Lights';
+        console.log(lightWaves);
+    }
+
+    console.log(lightWaves);
+};
+  
+logVisibleLightWaves();
