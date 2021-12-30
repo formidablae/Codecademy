7c2727e24321475ec8f1962ec7ea68a177b99bb3
formidablae <81068781+formidablae@users.noreply.github.com>
2021-12-30 22:22:55 +0100

    Did Career Path Full-Stack Engineer / 04 JavaScript Syntax Part II / 04.4 Learn JavaScript Syntax Iterators / 04.4.1 Higher-Order Functions

---
+++ Career_Path_Full-Stack_Engineer/04_JavaScript_Syntax_Part_II/04.4_Learn_JavaScript_Syntax_Iterators/04.4.1_Higher-Order_Functions/04.4.1.2_Functions_as_Data/app.js
@@ -0,0 +1,12 @@
+const checkThatTwoPlusTwoEqualsFourAMillionTimes = () => {
+    for (let i = 1; i <= 1000000; i++) {
+        if ((2 + 2) != 4) {
+            console.log('Something has gone very wrong :( ');
+        }
+    }
+}
+
+// Write your code below
+const is2p2 = checkThatTwoPlusTwoEqualsFourAMillionTimes;
+is2p2();
+console.log(is2p2.name);
