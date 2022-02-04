2b751575163d5372eb8f6902062962e6197ee40d
formidablae <81068781+formidablae@users.noreply.github.com>
2022-02-04 23:13:04 +0100

    Did Career Path Full-Stack Engineer / 05 Setting Up Your Dev Environment / 05.2 Command Line for Building Websites

---
+++ Career_Path_Full-Stack_Engineer/05_Setting_Up_Your_Dev_Environment/05.2_Command_Line_for_Building_Websites/05.2.5_Daily_Buzz/commands.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+pwd
+ls
+cd national/politics/
+ls
+mkdir elections
+cd elections/
+touch candidates.txt issues.txt
+cd ../../..
+mkdir business/
+cd business/
+ls
+mkdir startups
+cd startups
+cd ..
+mkdir disruptors
+touch disruptors/tech.txt disruptors/design.txt disruptors/education.txt
+cd ..
+pwd
